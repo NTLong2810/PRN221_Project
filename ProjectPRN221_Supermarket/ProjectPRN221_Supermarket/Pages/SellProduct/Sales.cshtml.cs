aa87575ef1fb9c78df6719250f55f9aed6572014
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-11-09 09:39:32 +0700

    Add Sale Page

---
+++ ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/Pages/SellProduct/Sales.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectPRN221_Supermarket.Models;
+using ProjectPRN221_Supermarket.Repository;
+
+namespace ProjectPRN221_Supermarket.Pages.SellProduct
+{
+    public class SalesModel : PageModel
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ISalesOrderRepository _salesOrderRepository;
+
+        public SalesModel(IProductRepository productRepository, ISalesOrderRepository salesOrderRepository)
+        {
+            _productRepository = productRepository;
+            _salesOrderRepository = salesOrderRepository;
+        }
+
+        [BindProperty]
+        public int ProductId { get; set; }
+
+        [BindProperty]
+        public int Quantity { get; set; }
+
+        public List<Product> Products { get; set; }
+        public List<SalesOrderItem> SalesOrderItems { get; set; }
+
+        public void OnGet()
+        {
+            Products = _productRepository.GetAllProducts();
+            SalesOrderItems = _salesOrderRepository.GetSalesOrderItems();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _salesOrderRepository.SellProduct(ProductId, Quantity);
+                return RedirectToPage();
+            }
+
+            Products = _productRepository.GetAllProducts();
+            SalesOrderItems = _salesOrderRepository.GetSalesOrderItems();
+            return Page();
+        }
+    }
+}
