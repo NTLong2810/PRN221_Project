80386f5106e91aaffb473b2a245a141304cb25dc
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-11-11 12:10:51 +0700

    Add SignalR

--- ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/wwwroot/js/site.js
+++ ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/productHub").build();
+
+connection.on("ReceiveMessage", function (message) {
+    console.log(message);
+    // Refresh or update UI as needed
+});
+
+connection.start().then(function () {
+    console.log("Connected to SignalR Hub");
+}).catch(function (err) {
+    console.error(err);
+});
