3fbb90ed4da9bd2039db5a5ee183be5eb7729eb8
NTLong2810 <112938446+NTLong2810@users.noreply.github.com>
2023-11-12 12:23:23 +0700

    Add SignalR for Sales

---
+++ ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/wwwroot/js/sale.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/productHub").build();
+
+connection.on("ReceiveSalesUpdate", function () {
+    console.log("Sales updated");
+    location.reload();
+});
+
+connection.on("ReceiveOutOfStockMessage", function (message) {
+    console.log("Out of stock message received:", message);
+    alert(message);  // Thông báo cho người dùng khi hết hàng
+});
+
+
+connection.start().then(function () {
+    console.log("Connected to SignalR Hub");
+}).catch(function (err) {
+    console.error(err);
+});
--- ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/wwwroot/js/site.js
+++ ProjectPRN221_Supermarket/ProjectPRN221_Supermarket/wwwroot/js/site.js
@@ -1,13 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-"use strict";
+﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/productHub").build();
 
-connection.on("ReceiveMessage", function (message) {
-    console.log(message);
+connection.on("ReceiveChangeProduct", function () {
+    console.log("Product updated");
+    location.reload();
     // Refresh or update UI as needed
 });
 
